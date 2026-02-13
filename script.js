7130b33e86b0552f79d1d651a0b9a8a0de9eeed2
LucasFortunato121 <lucasfortunato10276@gmail.com>
2026-02-13 10:38:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+alert("Faça seu login")
+confirm("Seus dados se login serão guardados!")
+function oo(){
+var p1 = document.getElementById("ip1").value
+var p2 = document.getElementById("ip2").value
+if(p1=== "adm" && p2 === "6789"){
+alert("BEM VINDO " + p1)
+window.location.href="https://lucasfortunato10276-bot.github.io/trabalhoADM/"
+
+
+
+}
+else{
+alert("Senha ou Username Incorretos!")
+
+
+
+
+}
+
+
+
+}
+
+
+
+
+
+
